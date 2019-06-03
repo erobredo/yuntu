96a39074ad99551f4f461271731c290866b26db4
Everardo Robredo <erobredo@conabio.gob.mx>
2019-06-03 23:16:57 +0000

    Remove trash

--- yuntu/core/datastore/.ipynb_checkpoints/utils-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-import json
-
-def hashDict(dDict):
-    #Not reliable for non str keys (tuples, etc)
-    m = hashlib.md5(json.dumps(dDict, sort_keys=True, ensure_ascii=True).encode('utf-8'))
-
-    return m.hexdigest()
