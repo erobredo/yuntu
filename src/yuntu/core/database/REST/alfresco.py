287f992f07ebcdff66d02b2faeb07e2a6da63ffb
erobredo <everardo.robredo@gmail.com>
2023-03-07 10:32:44 -0600

    Update alfresco.py

--- src/yuntu/core/database/REST/alfresco.py
+++ src/yuntu/core/database/REST/alfresco.py
@@ -151,7 +151,7 @@ class AlfrescoREST(RESTManager):
         self.recording_parser = config["recording_parser"]
 
     def build_recordings_url(self):
-        return f"{self.api_url}/search/"
+        return f"{self.api_url}"
 
     def build_models(self):
         """Construct all database entities."""
